7fb248bccfeff4f18044dbe00489ce37800c99b9
unknown <kredlapa@L7-CNU24890J0.amd.com>
2015-09-29 15:17:43 +0530

    to git

---
+++ Java/src/hacker_rank_submissions/algorithms/warmup/plus_minus.java
@@ -0,0 +1,16 @@
+package hacker_rank_submissions.algorithms.warmup;
+
+/**
+ * Created by kredlapa on 9/29/2015.
+ */
+public class plus_minus
+{
+    public static void main(String[] args)
+    {
+        int positive=0,negative=0,zero=0;
+        float float_positive=0,float_negative=0,float_zero=0;
+
+        System.out.println("in main");
+
+    }
+}
