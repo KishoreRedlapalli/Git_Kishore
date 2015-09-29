435ec04e70a024daa153712dc86bbc9273368f29
Kishore Redlapalli <Kishore.Redlapalli@amd.com>
2015-09-29 18:02:22 +0530

    to git

---
+++ Java/src/hacker_rank_submissions/algorithms/warmup/test.java
@@ -0,0 +1,11 @@
+package hacker_rank_submissions.algorithms.warmup;
+import java.lang.*;
+/**
+ * Created by kredlapa on 9/29/2015.
+ */
+public class test {
+    public static void main(String[] args)
+    {
+        System.out.println("hello world");
+    }
+}
