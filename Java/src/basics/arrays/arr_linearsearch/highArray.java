4fdf0c81535889aa7ac8596eb41dc05c565a8e7a
Kishore Redlapalli <Kishore.Redlapalli@amd.com>
2016-01-05 10:28:08 +0530

    togit

--- Java/src/basics/arrays/arr_linearsearch/highArray.java
+++ Java/src/basics/arrays/arr_linearsearch/highArray.java
@@ -31,4 +31,69 @@ public class highArray {
         arr[nElems] = value;
         nElems++;
     }
+    public void display()
+    {
+        for (int i=0;i<nElems;i++)
+        {
+            System.out.println(arr[i]+"\t");
+        }
+    }
+    public boolean delete(long valToBeDeleted)
+    {
+        int j;
+        for(j=0;j<nElems;j++)
+            if(arr[j]==valToBeDeleted)//lookforit
+                break;
+        if(j==nElems)
+            return false;//can't find it
+        else
+        {
+            for(int k=j;k<nElems;k++)
+            {
+                arr[k]=arr[k+1];
+            }
+            nElems--;
+            return true;
+        }//end of delete method
+    }
+    public void getmax()
+    {
+        System.out.println("in getmax method");
+        long max=arr[0];
+        for(int i=0;i<nElems;i++)
+        {
+            if (arr[i]>=max)
+            {
+                max=arr[i];
+            }
+        }
+        System.out.println("max element is "+max);
+    }
+}
+class HighArrayApp
+{
+    public static void main(String[] args)
+    {
+        int maxSize=100;
+        highArray arr;
+        arr =new highArray(maxSize);
+        arr.insert(100);
+        arr.insert(77);
+        arr.insert(1002);
+        arr.insert(99);
+        arr.insert(44);
+        arr.insert(55);
+        arr.insert(22);
+        arr.insert(88);
+        arr.insert(11);
+        arr.insert(00);
+        arr.insert(66);
+        arr.insert(33);
+        arr.insert(1002);
+        arr.insert(10055);
+        arr.display();
+        arr.getmax();
+
+
+    }
 }
