dd2d7d4159c93bc097a79e87ddb1759f578776a5
Kishore Redlapalli <kishore007@gmail.com>
2016-02-03 20:58:33 +0530

    togit

--- Java/src/SLL_Gen_nonRecursive/Link.java
+++ Java/src/SLL_Gen_nonRecursive/Link.java
@@ -3,5 +3,19 @@ package SLL_Gen_nonRecursive;
 /**
  * Created by KishoreRP on 10/18/2015.
  */
-public class Link {
+public class Link<E>
+{
+    E carname;
+    E carmake;
+    public Link nextLink;
+    public Link(E name,E make)
+    {
+        this.carname=name;
+        this.carmake=make;
+    }
+    public void printLink()
+    {
+        System.out.println(" carname\t"+carname+"\tcarmake\t"+carmake);
+    }
+
 }
--- Java/src/SLL_Gen_nonRecursive/LinkList.java
+++ Java/src/SLL_Gen_nonRecursive/LinkList.java
@@ -1,7 +1,14 @@
 package SLL_Gen_nonRecursive;
-
+import java.util.*;
 /**
  * Created by KishoreRP on 10/18/2015.
  */
-public class LinkList {
+public class LinkList<E>
+{
+    private Link<E> first;
+    public void LinkList()
+    {
+        first=null;
+    }
+
 }
